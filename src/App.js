97d105e3f57fe53bd896a318d5763726c2c0a6ee
crystaljessica <a@b.c>
2022-03-27 10:28:21 -0700

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import SideBar from "./Sidebar";
+import Chat from './Chat';
+import { useEffect, useState } from 'react';
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		axios.get("/messages/sync").then((response) => {
+			setMessages(response.data);
+		});
+	}, []);
+
+
+
+  useEffect(()=>{
+    const pusher = new Pusher('b70c9dc33cd1fedb799f', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('message');
+    channel.bind('inserted', function(newMessage) {
+      setMessages([...messages, newMessage]);
+    });
+
+    // Clean up function
+		return () => {
+			channel.unbind_all();
+			channel.unsubscribe("message");
+		};
+  },[messages])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__body">
+        <SideBar />
+        <Chat messages={messages}/>
+      </div>
+      
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABiYMzqDWq2DLLDQghqJxlbAcKo0pro8I",
+    authDomain: "what-sapp-mern-c063d.firebaseapp.com",
+    projectId: "what-sapp-mern-c063d",
+    storageBucket: "what-sapp-mern-c063d.appspot.com",
+    messagingSenderId: "643358608043",
+    appId: "1:643358608043:web:1fd7a9811d7177f827c5ee"
+  };
